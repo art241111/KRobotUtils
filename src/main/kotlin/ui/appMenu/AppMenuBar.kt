eeccc6606058620022996ce6b153ff45a30cb753
artem_admin <art241111@mail.ru>
2022-01-17 11:31:39 +0300

    Redesign app bar menu

--- src/main/kotlin/ui/appMenu/AppMenuBar.kt
+++ src/main/kotlin/ui/appMenu/AppMenuBar.kt
@@ -1,13 +1,18 @@
 package ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Divider
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.FrameWindowScope
 import data.AppBarMenuItem
 import data.AppBarMenuItemWithContext
@@ -20,54 +25,50 @@ fun AppMenuBar(
     appBarMenuItems: List<AppBarMenuItem>,
     scope: FrameWindowScope
 ) {
-    Row {
-        appBarMenuItems.forEach {
-            val expanded = remember { mutableStateOf(false) }
+    Column {
+        Divider(Modifier.fillMaxWidth())
+        Row() {
+            appBarMenuItems.forEach {
+                val expanded = remember { mutableStateOf(false) }
 
-            if (it is AppBarMenuList) {
-                Box {
-                    MenuBarItem(
-                        text = it.itemText,
-                        onClick = {
-                            expanded.value = true
-                        }
-                    )
+                if (it is AppBarMenuList) {
+                    Box {
+                        MenuBarItem(
+                            text = it.itemText,
+                            onClick = {
+                                expanded.value = true
+                            }
+                        )
 
-                    DropdownMenu(
-                        expanded = expanded.value,
-                        onDismissRequest = { expanded.value = false }
-                    ) {
-                        it.list.forEach {
-                            DropdownMenuItem(
-                                onClick = {
-                                    expanded.value = false
-                                }
-                            ) {
-                                if (it is SimpleAppBarMenuItem) {
-                                    MenuBarItem(
-                                        text = it.itemText,
-                                        onClick = {
+                        DropdownMenu(
+                            expanded = expanded.value,
+                            onDismissRequest = { expanded.value = false }
+                        ) {
+                            it.list.forEach {
+                                DropdownMenuItem(
+                                    onClick = {
+                                        expanded.value = false
+
+                                        if (it is SimpleAppBarMenuItem) {
                                             it.onClick()
-                                            expanded.value = false
-                                        }
-                                    )
-                                } else if (it is AppBarMenuItemWithContext) {
-                                    MenuBarItem(
-                                        text = it.itemText,
-                                        onClick = {
+                                        } else if (it is AppBarMenuItemWithContext) {
                                             it.onClick(scope)
-                                            expanded.value = false
                                         }
-                                    )
+                                    }
+                                ) {
+                                    Text(it.itemText)
                                 }
                             }
                         }
                     }
+
                 }
 
+
             }
-
-
         }
+
+        Divider(Modifier.fillMaxWidth())
     }
+
 }
